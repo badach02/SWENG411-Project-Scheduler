252f3e8f160884d131ea8566217ac1c6eb133ba3
Daniel <dxb718@psu.edu>
2025-11-04 09:33:22 -0500

    urls.py changes, static/images/

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("main.urls")),
-    path("register/", include("main.urls")),
+    path('', include("main.urls")),
     path('admin/', admin.site.urls),
 ]
