0f4ccf0d2d1778535e33e1bc400d0efb19af97a8
Daniel <dxb718@psu.edu>
2025-11-04 09:15:19 -0500

    shift model changes

--- main/models.py
+++ main/models.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 from django.db import models
+from datetime import date, time
+
+ROLES = {
+    "MGR": "Manager",
+    "CK": "Cook",
+    "SVR": "Server",
+}
 
 class account(AbstractUser):
     account_type = models.CharField(max_length=20)
@@ -8,7 +16,12 @@ class account(AbstractUser):
         return f"{self.username} | {self.account_type}"
     
 class shift(models.Model):
-    date = models.DateField
-    time = models.TimeField
-    role = models.CharField(max_length=30)
+    date = models.DateField(default=date.today)
+    start_time = models.TimeField(default=timezone.now)
+    end_time = models.TimeField(default=timezone.now)
+    role = models.CharField(choices=ROLES, default="Server")
+    cover_employee_id = models.IntegerField(default=0)
 
+    def __str__(self):
+        return f"Shift on {self.date} from {self.start_time} to {self.end_time} doing: {self.role}"
+    
