813a1fb2833a73b6826f02849559f6c6ffd5567b
ijz5023 <ijz5023@psu.edu>
2025-12-05 14:20:44 -0500

    Updated for assignment of roles upon registration validation.

--- main/urls.py
+++ main/urls.py
@@ -16,9 +16,11 @@ urlpatterns = [
     path('settings/', views.settings_view, name='account_settings'),
     path('initialization/', views.initialize_view, name='initialization'),
     path('api/users/', views.get_users, name= "get_users"),
-    
+
+
     # manager/
     path('manager/', views.manager_view, name='manager'),
     path('manager/requests', views.manage_requests_view, name='requests_manager'),
     path('manager/scheduler', views.scheduler_view, name='scheduler'),
+    path('manager/requests/<int:request_id>/', views.registration_decision_view, name='registration_decision'),
 ]
